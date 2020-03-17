35e2238a2f6b118927aca5e57ce571134271153c
Crystal Ly <crystal.m.ly@gmail.com>
2020-03-16 20:56:49 -0500

    added php code that will render "Hello World"

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <html>
+
 <head>
-<title>My first PHP page</title>
+    <title>My first PHP page</title>
 </head>
 
 <body>
 
+    <!-- This will be parsed by PHP and will output the echo bit. -->
+    <?php
 
+    echo '<p>Hello World</p>';
+
+    ?>
 
 
 </body>
