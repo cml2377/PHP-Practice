696986a3c1c3373688112c9044e6b96ec9aba397
Crystal Ly <crystal.m.ly@gmail.com>
2020-03-16 21:36:10 -0500

    added php to PATH environment variable, and removed HTML from index.php

--- index.php
+++ index.php
@@ -1,29 +1,13 @@
-<html>
+<?php
 
-<head>
-    <title>Can't Outrun the Fork</title>
-</head>
+/* Within the php brackets, comments look just like JavaScript. */
 
-<body>
+echo "Meal and Calorie Counter\n";
 
-    <!-- This will be parsed by PHP and will output the echo bit. -->
-    <?php
+// Variables all start with $ and they are case-sensitive.
 
-    /* Within the php brackets, comments look just like JavaScript. */
+$name = 'Crystal Ly';
 
-    echo '<h1>Meal and Calorie Counter</h1>';
+echo "Hi, I am $name and this is my first php/Laravel project.";
 
-    // Variables all start with $ and they are case-sensitive.
-
-    $name = 'Crystal Ly';
-
-    echo "<p>Hi, I am $name and this is my first php/Laravel project.</p>"
-
-    // There is a difference in double-quote and single quote when you insert variables into a string.
-
-    ?>
-
-
-</body>
-
-</html>
+// There is a difference in double-quote and single quote when you insert variables into a string.
